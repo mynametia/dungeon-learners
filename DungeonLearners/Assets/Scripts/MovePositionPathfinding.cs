d767df80a8c80ddd8b8018c54ff5333384af13ee
Gao Sitian <gaositian4966@gmail.com>
2022-02-22 16:35:59 +0800

    Added AI pathfinding grid

---
+++ DungeonLearners/Assets/Scripts/MovePositionPathfinding.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositionPathfinding : MonoBehaviour
+{
+    public void SetMovePosition(Vector3 movePosition)
+    { 
+        
+    }
+}
