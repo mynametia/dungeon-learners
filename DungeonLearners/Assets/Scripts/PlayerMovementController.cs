aa5a023a03e8a8fbdb1d3e7f6acc2c232250b7ae
Gao Sitian <gaositian4966@gmail.com>
2022-02-22 18:08:52 +0800

    Added player movement

---
+++ DungeonLearners/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public GameObject playerMovementDestination;
+    public GameObject currentDestination;
+    void Start()
+    {
+        if (currentDestination == null)
+        {
+            currentDestination = Instantiate(playerMovementDestination);
+        }
+        currentDestination.transform.position = transform.position;
+        GetComponent<AIDestinationSetter>().target = currentDestination.transform;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            currentDestination.transform.position = touchPosition;
+        }
+    }
+}
---
+++ DungeonLearners/Assets/Scripts/PlayerSpriteController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PlayerSpriteController : MonoBehaviour
+{
+    public AIPath aiPath;
+    //-1 if the sprite is default facing left, 1 if the sprite is default facing right
+    public int defaultFaceRight = 1;
+    private float initialScaleX, initialScaleY, initialScaleZ;
+
+    void Start()
+    {
+        initialScaleX = transform.localScale.x;
+        initialScaleY = transform.localScale.y;
+        initialScaleZ = transform.localScale.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x <= -0.01f) //sprite face left
+        {
+            transform.localScale = new Vector3(-defaultFaceRight*initialScaleX, initialScaleY, initialScaleZ);
+        }
+        else if (aiPath.desiredVelocity.x >= 0.01f) //sprite face right
+        {
+            transform.localScale = new Vector3(defaultFaceRight * initialScaleX, initialScaleY, initialScaleZ);
+        }
+
+    }
+}
