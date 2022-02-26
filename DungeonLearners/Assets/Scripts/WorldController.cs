dea4769ae543e10e7abaaae63abd7081ae299cb5
Gao Sitian <gaositian4966@gmail.com>
2022-02-26 19:00:57 +0800

    Added logic to procedurally generate dungeon entrances

--- DungeonLearners/Assets/Scripts/WorldController.cs
+++ DungeonLearners/Assets/Scripts/WorldController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WorldController : MonoBehaviour
 {
-    // Spawns dungeons and layers in world based on number of dungeons 
+    // Dynamically spawns layers in world based on number of dungeons 
 
     public GameObject worldLayer;
     public GameObject worldLayerLast;
@@ -23,22 +23,34 @@ public class WorldController : MonoBehaviour
         // Number of layers in world
         layerCount = Mathf.CeilToInt((float) dungeonCount / dungeonPerLayer);
 
+        if (layerCount == 0) { layerCount = 1; }
+
         Debug.Log("no. of layers: " + layerCount);
 
         GameObject layer;
         // Procedurally generate layers in world
         for (int i = 0; i < layerCount; i++)
         {
-            int layerDungeonCount;
             if (i == layerCount - 1) // Generate last layer
             {
-                layerDungeonCount = dungeonCount % dungeonPerLayer;
                 layer = Instantiate(worldLayerLast, transform);
+                if (dungeonCount % dungeonPerLayer != 0)
+                {
+                    layer.GetComponent<WorldLayerController>().dungeonCount = dungeonCount % dungeonPerLayer;
+                }
+                else if (dungeonCount != 0)
+                {
+                    layer.GetComponent<WorldLayerController>().dungeonCount = dungeonPerLayer;
+                }
+                else
+                {
+                    layer.GetComponent<WorldLayerController>().dungeonCount = 0;
+                }
             }
             else // Generate middle layers
             {
-                layerDungeonCount = dungeonPerLayer;
                 layer = Instantiate(worldLayer, transform);
+                layer.GetComponent<WorldLayerController>().dungeonCount = dungeonPerLayer;
             }
             layer.transform.localPosition = new Vector3(0, i * layerOffset, 0);
         }
---
+++ DungeonLearners/Assets/Scripts/WorldLayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldLayerController : MonoBehaviour
+{
+    // Dynamically spawns dungeons per layer
+
+    public int dungeonCount = 0;
+    private static int maxDungeonCount = 4, dungeonEntranceOffset = 5, firstDungeonXPos = -8, dungeonYPos = 7;
+
+    public GameObject groundLayer;
+    public GameObject dungeonEntrance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dungeonCount > maxDungeonCount) { dungeonCount = maxDungeonCount; }
+        else if (dungeonCount < 0) { dungeonCount = 0; }
+        
+        // Instantiate dungeon entrances from left to right
+        int count = 0;
+        GameObject entrance;
+        while (count < dungeonCount)
+        {
+            entrance = Instantiate(dungeonEntrance, groundLayer.transform);
+            entrance.transform.localPosition = new Vector3(firstDungeonXPos + count * dungeonEntranceOffset, dungeonYPos, 0);
+            count++;
+        }
+
+    }
+
+}
