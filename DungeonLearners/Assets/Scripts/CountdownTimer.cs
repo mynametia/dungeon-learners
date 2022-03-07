f851559f65ceb2d541a4ff397dfa2219c77c3caf
Shang Zi Yuan <happy.red.bear@gmail.com>
2022-03-07 19:40:43 +0800

    Added CountdownText UI and CountdownTimer script

---
+++ DungeonLearners/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
