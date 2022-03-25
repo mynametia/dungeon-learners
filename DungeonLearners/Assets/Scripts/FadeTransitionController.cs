88fa36bee15462b6fccda200d38b5a3e190f42e8
Gao Sitian <gaositian4966@gmail.com>
2022-03-25 15:07:05 +0800

    Added Scene Transition from Create/Edit Dungeon

--- DungeonLearners/Assets/Scripts/FadeTransitionController.cs
+++ DungeonLearners/Assets/Scripts/FadeTransitionController.cs
@@ -6,11 +6,18 @@ public class FadeTransitionController : MonoBehaviour
     public Animator fadeAnimator;
     //public GameObject Player;
     private static string sceneToLoad;
+    private static Scene lastScene;
+
+    public void ReturnToPreviousScene()
+    {
+        FadeToBlack(lastScene.name);
+    }
 
     // Triggers fade to black animation
     public void FadeToBlack(string sceneName)
     {
         //Player = null;
+        lastScene = SceneManager.GetActiveScene();
         sceneToLoad = sceneName;
         fadeAnimator.SetTrigger("FadeOut");
     }
