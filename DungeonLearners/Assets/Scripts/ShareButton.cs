62b89f05a92b074e3bb052c39fe8fa8d366c337e
Gao Sitian <gaositian4966@gmail.com>
2022-03-25 15:13:55 +0800

    Added different sharing message for user-created world vs other ppl's world

--- DungeonLearners/Assets/Scripts/ShareButton.cs
+++ DungeonLearners/Assets/Scripts/ShareButton.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 public class ShareButton : MonoBehaviour
 {
-
+    public bool myWorld = false;
     // private String shareMessage;
 
     // public void ClickShareButton(){
@@ -21,24 +21,34 @@ public class ShareButton : MonoBehaviour
     }
 
 
-private IEnumerator TakeScreenshotAndShare()
-{
-    yield return new WaitForEndOfFrame();
+    private IEnumerator TakeScreenshotAndShare()
+    {
+        yield return new WaitForEndOfFrame();
 
-    Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
-    ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
-    ss.Apply();
+        Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+        ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+        ss.Apply();
 
-    string filePath = Path.Combine( Application.temporaryCachePath, "shared img.png" );
-    File.WriteAllBytes( filePath, ss.EncodeToPNG() );
+        string filePath = Path.Combine( Application.temporaryCachePath, "shared img.png" );
+        File.WriteAllBytes( filePath, ss.EncodeToPNG() );
 
-    // To avoid memory leaks
-    Destroy( ss );
+        // To avoid memory leaks
+        Destroy( ss );
 
-    new NativeShare().AddFile( filePath )
-        .SetSubject( "Dungeon Learners" ).SetText( "Hey Guys! Come join me in playing Dungeon Learners. Here's my rank! " ).SetUrl( "https://github.com/yasirkula/UnityNativeShare" )
-        .SetCallback( ( result, shareTarget ) => Debug.Log( "Share result: " + result + ", selected app: " + shareTarget ) )
-        .Share();
+        string msg;
+        if (myWorld)
+        {
+            msg = "Hey Guys! Come join me in playing Dungeon Learners. Here's a world I created! ";
+        }
+        else 
+        {
+            msg = "Hey Guys! Come join me in playing Dungeon Learners. Here's my rank! ";
+        }
+
+        new NativeShare().AddFile( filePath )
+            .SetSubject( "Dungeon Learners" ).SetText( msg ).SetUrl( "https://github.com/yasirkula/UnityNativeShare" )
+            .SetCallback( ( result, shareTarget ) => Debug.Log( "Share result: " + result + ", selected app: " + shareTarget ) )
+            .Share();
 
         
     }
