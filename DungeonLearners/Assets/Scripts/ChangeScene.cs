4a9515965671c0f732f6738b0a2b75a6d8111851
Gao Sitian <gaositian4966@gmail.com>
2022-03-26 01:19:15 +0800

    Fixed bugs in transiting from create/edit world to create/edit dungeon

--- DungeonLearners/Assets/Scripts/ChangeScene.cs
+++ DungeonLearners/Assets/Scripts/ChangeScene.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public static string previousScene;
     public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
+    }
+
+    public static void MoveToScene(string sceneName)
+    {
+        previousScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public static void ReturnPreviousScene()
+    {
+        Debug.Log(previousScene);
+        MoveToScene(previousScene);
     }
 }
