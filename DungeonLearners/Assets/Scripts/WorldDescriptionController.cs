36d4e34ce780c6217704232e723d59e7e1747d24
Aditya Bansal <adityabansal10@gmail.com>
2022-03-28 04:59:35 +0800

    Updates myWorldDescription

--- DungeonLearners/Assets/Scripts/WorldDescriptionController.cs
+++ DungeonLearners/Assets/Scripts/WorldDescriptionController.cs
@@ -1,21 +1,30 @@
 
 using UnityEngine;
+using TMPro;
 
 public class WorldDescriptionController : MonoBehaviour
 {
     public GameObject description, leaderboard;
     public GameObject leaderBoardEntry, rankingBoard;
-        
+    public TMP_Text worldName, worldID;
+    
     private int playerNumber = 10;
+
 
     void Start()
     {
         GameObject rank;
+
         for (int i = 1; i <= playerNumber; i++)
         {
             rank = Instantiate(leaderBoardEntry, rankingBoard.transform);
             rank.GetComponent<RankDataController>().setRanking(i);
         }
+
+        TMP_Text desc = description.GetComponentInChildren<TMP_Text>();
+        desc.text = GameState.getCurrentWorld().description;
+        worldName.text = GameState.getCurrentWorld().worldName;
+        worldID.text = GameState.getCurrentWorld().worldID.ToString();
     }
 
     public void CloseWindow()
