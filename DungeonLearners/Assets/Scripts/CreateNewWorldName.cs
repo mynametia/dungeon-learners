6bc371e18345c9c44b25a8d502037928e1b7d066
Aditya Bansal <adityabansal10@gmail.com>
2022-03-27 14:40:41 +0800

    Adds create and edit world scripts

--- DungeonLearners/Assets/Scripts/CreateNewWorldName.cs
+++ DungeonLearners/Assets/Scripts/CreateNewWorldName.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
+using Firebase;
+using Firebase.Database;
+using Firebase.Firestore;
+using static Firebase.Firestore.DocumentReference;
 public class CreateNewWorldName : MonoBehaviour
 {
     public TMP_InputField worldName1;
@@ -18,9 +23,41 @@ public class CreateNewWorldName : MonoBehaviour
         world_descOne = worldDesc1.text;
         PlayerPrefs.SetString("WorldDesc1", world_descOne);
         PlayerPrefs.SetString("WorldName1", world_nameOne); 
-        SceneManager.LoadScene(4);
+
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        World world = new World(world_nameOne, world_descOne, "Public");
+
+        GameState.setCurrentWorld(world);
+
+        string json = JsonUtility.ToJson(world);
+        string worldId = GameState.getCurWorldID().ToString();
+        print(worldId.GetType());
+        reference.Child("worlds").Child(worldId).SetRawJsonValueAsync(json);
+
+
+        // var worldVar = new WorldData{
+            
+        //     worldName = world_nameOne, 
+        //     worldDescription = world_descOne, 
+        //     visibility = "Public"
+        // };
+
+        // var firestore = FirebaseFirestore.DefaultInstance;
+        // // DocumentReference docRef = firestoreDB.Collection("question_bank").Document(world_nameOne);
+        
+        // firestore.Document("question_bank").SetAsync(worldVar);
+        
+        // Dictionary<string, object> city = new Dictionary<string, object>
+        // {
+        //         // { "Name", "Los Angeles" },
+        //         // { "State", "CA" },
+        //         // { "Country", "USA" }
+        // };
+        // docRef.SetAsync(city).ContinueWithOnMainThread(task => {
+        //         Debug.Log("Added world to the collection");
+        // });
+
+        SceneManager.LoadScene("World Manager");
 
     }
-
- 
 }
--- DungeonLearners/Assets/Scripts/EditWorldController.cs
+++ DungeonLearners/Assets/Scripts/EditWorldController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+using UnityEngine.SceneManagement;
+
+using Firebase;
+using Firebase.Database;
+
 public class EditWorldController : MonoBehaviour
 {
     public TMP_InputField text1;
@@ -17,5 +22,26 @@ public class EditWorldController : MonoBehaviour
         text1.text = world1;
         worlddesc1 = PlayerPrefs.GetString("WorldDesc1");
         desc1.text = worlddesc1;
+    }
+
+    public void SaveWorldName()
+    {
+        world1 = text1.text;
+        worlddesc1 = desc1.text;
+        PlayerPrefs.SetString("WorldDesc1", worlddesc1);
+        PlayerPrefs.SetString("WorldName1", world1); 
+
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        World world = new World(world1, worlddesc1, "Public");
+
+        GameState.setCurrentWorld(world);
+
+        string json = JsonUtility.ToJson(world);
+        string worldId = GameState.getCurWorldID().ToString();
+        print(worldId.GetType());
+        reference.Child("worlds").Child(worldId).SetRawJsonValueAsync(json);
+
+        SceneManager.LoadScene("World Manager");
+
     }
 }
