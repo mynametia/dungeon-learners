38877ac3c51756d1663f89f3a86f68fc49dbf16d
afiqaharis <afiqaharis@gmail.com>
2022-03-29 13:41:26 +0800

    Update ScoreManager.cs

--- DungeonLearners/Assets/Scripts/ScoreManager.cs
+++ DungeonLearners/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -9,9 +11,48 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DatabaseReference DBreference = FirebaseDatabase.DefaultInstance.RootReference;
         score = PlayerPrefs.GetInt("EXP");
         coins = PlayerPrefs.GetInt("Coins");
+
+        StartCoroutine(UpdateCoins(int.Parse(coins)));
+        StartCoroutine(UpdateScore(int.Parse(score)));
     }
+
+    private IEnumerator UpdateCoins (int _coins)
+    {
+        //Set the currently logged in user
+        var DBTask = DBreference.Child("users").Child(User.UserName).Child("coins").SetValueAsync(_coins);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //coins is now updated
+        }
+    }
+
+    private IEnumerator UpdateScore (int _score)
+    {
+        //Set the currently logged in user
+        var DBTask = DBreference.Child("wolrds").Child(User.UserName).Child("EXP").SetValueAsync(_score);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //Score is now updated
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
