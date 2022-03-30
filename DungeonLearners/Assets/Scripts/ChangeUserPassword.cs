fe3cf59b158411274a11d059a80d399507537ee5
Willis Yang <willisyang7@gmail.com>
2022-03-31 01:43:08 +0800

    change password scripts

---
+++ DungeonLearners/Assets/Scripts/ChangeUserPassword.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class ChangeUserPassword : MonoBehaviour
+{
+    //Firebase Variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth; 
+
+    //Change Password Variables
+    [Header("Change Password")]
+    public TMP_InputField NewPasswordField;
+    public TMP_InputField confirmNewPasswordField;
+    public TMP_Text newpasswordWarningText;
+    public TMP_Text newpasswordConfirmText;
+
+    public IEnumerator ChangePasswordButton()
+    {
+        if (NewPasswordField.text != confirmNewPasswordField.text)
+        {
+            newpasswordWarningText.text = "Passwords do not match, try again!";
+        }
+        else
+        {
+            CheckPasswordCondition();
+            newpasswordChange(NewPasswordField.text);
+            auth.SignOut();
+            newpasswordConfirmText.text = "Password change successful! Please login again.";
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene("Login");
+        }
+    }
+
+    void newpasswordChange(string NewPasswordField)
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user!=null)
+        {
+            user.UpdatePasswordAsync(NewPasswordField).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("Update Password was cancelled");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("Update Password was faulted");
+                    return;
+                }
+
+                Debug.Log("Password Update is successful!");
+            });
+        }
+    }
+
+    public void CheckPasswordCondition()
+    {
+        string ReceivedString = NewPasswordField.text;
+        if (ReceivedString.Any(char.IsLetter) && ReceivedString.Any(char.IsDigit) && ReceivedString.Any(char.IsUpper) && 
+        ReceivedString.Any(ch => ! char.IsLetterOrDigit (ch)) && ReceivedString.Length > 8)
+        {
+            Debug.Log("Password is good, allowed to register");
+        }
+        else
+        {
+            Debug.Log("Password is bad, registration is not allowed");
+            newpasswordWarningText.text = "Weak Password. Please make sure it contains digits, letters and special characters";
+        }
+    }
+}
