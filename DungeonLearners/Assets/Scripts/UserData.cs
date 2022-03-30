145be34c69b097697a801231df3996701993f5b7
Willis Yang <willisyang7@gmail.com>
2022-03-30 23:57:11 +0800

    new scripts for display in account info

---
+++ DungeonLearners/Assets/Scripts/UserData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+using Firebase.Database;
+using Firebase.Extensions;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class UserData : MonoBehaviour
+{
+    public static string username;
+    public static string email;
+    public static string coins;    
+}
