a46e9538120ee3ae16e67d1af8801565c452740f
Shang Zi Yuan <happy.red.bear@gmail.com>
2022-03-30 11:20:50 +0800

    Changed questions back to using Firestore

--- DungeonLearners/Assets/Scripts/BattleQuestionController.cs
+++ DungeonLearners/Assets/Scripts/BattleQuestionController.cs
@@ -31,10 +31,10 @@ public class BattleQuestionController : MonoBehaviour
     {
         // Get dungeon room ID
         int curDungeonRoomID = DungeonController.currentDungeonRoomID;
-        addHardcodedQuestions("Computing", "Artificial Intelligence", curDungeonRoomID);
+        // addHardcodedQuestions("Computing", "Artificial Intelligence", curDungeonRoomID);
         // addDefaultQuestions();
-        // addQuestions("Computing", "Artificial Intelligence", 0);
-        currentQuestions = new List<Question>(battleQuestions);
+        addQuestions("Computing", "Artificial Intelligence", curDungeonRoomID);
+        // currentQuestions = new List<Question>(battleQuestions);
     }
 
     // Display correct answer text and remove submit button
