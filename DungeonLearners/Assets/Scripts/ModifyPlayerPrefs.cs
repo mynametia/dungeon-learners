fb200f5241814f1044fd915443ff5760303b8b1b
Shang Zi Yuan <happy.red.bear@gmail.com>
2022-03-30 21:53:03 +0800

    Add script to control PlayerPrefs and modified FindWorld scene

---
+++ DungeonLearners/Assets/Scripts/ModifyPlayerPrefs.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ModifyPlayerPrefs : MonoBehaviour
+{
+    public TMP_Text preloadWorldName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setPreloadWorldChoice()
+    {
+       Debug.Log(preloadWorldName.text);
+       PlayerPrefs.SetString("preloadWorldChoice", preloadWorldName.text);
+    }
+}
